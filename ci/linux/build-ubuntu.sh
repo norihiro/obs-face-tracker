a665495c6f811a371d111cf61ac4de704d0c9181
Norihiro Kamae <norihiro@nagater.net>
2021-06-18 22:16:15 +0900

    ci(linux): Fix libdir for Ubuntu
    Plugin .so file should be placed under `/usr/lib/obs-plugins/`.
    Somehow CMAKE_INSTALL_FULL_LIBDIR is not configurable by cmake `-D`
    option, I overwrite CMakeLists.txt by sed.

--- ci/linux/build-ubuntu.sh
+++ ci/linux/build-ubuntu.sh
@@ -11,6 +11,8 @@ cd ..
 cp LICENSE data/LICENSE-plugin
 cp dlib/LICENSE.txt data/LICENSE-dlib
 
+sed -i 's;${CMAKE_INSTALL_FULL_LIBDIR};/usr/lib;' CMakeLists.txt
+
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
