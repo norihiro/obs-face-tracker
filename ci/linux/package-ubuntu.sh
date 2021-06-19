b49df07d4624282670eefb092ea2e019825376c0
Norihiro Kamae <norihiro@nagater.net>
2021-06-19 12:36:43 +0900

    ci: new template

--- ci/linux/package-ubuntu.sh
+++ ci/linux/package-ubuntu.sh
@@ -5,12 +5,7 @@ set -e
 script_dir=$(dirname "$0")
 source "$script_dir/../ci_includes.generated.sh"
 
-export GIT_HASH=$(git rev-parse --short HEAD)
-export PKG_VERSION="1-$GIT_HASH-$BRANCH_SHORT_NAME-git"
-
-if [[ "$BRANCH_FULL_NAME" =~ "^refs/tags/" ]]; then
-	export PKG_VERSION="$BRANCH_SHORT_NAME"
-fi
+export PKG_VERSION="1-$(git describe --tags --long --always)"
 
 cd ./build
 
--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -3,7 +3,7 @@ call "%~dp0..\ci_includes.generated.cmd"
 mkdir package
 cd package
 
-git rev-parse --short HEAD > package-version.txt
+git describe --tags --long --always > package-version.txt
 set /p PackageVersion=<package-version.txt
 del package-version.txt
 
