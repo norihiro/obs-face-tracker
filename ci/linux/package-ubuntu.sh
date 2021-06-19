a06f2935b4c6ae90104d2f18155e93409e0a1cdb
Norihiro Kamae <norihiro@nagater.net>
2021-06-19 11:35:25 +0900

    ci(linux): detect version of required obs-studio

--- ci/linux/package-ubuntu.sh
+++ ci/linux/package-ubuntu.sh
@@ -9,12 +9,14 @@ export PKG_VERSION="1-$(git describe --tags --long --always)"
 
 cd ./build
 
+OBS_VER="$(dpkg -s obs-studio | awk '$1=="Version:"{print gensub(/^([0-9]*\.[0-9]*)\..*/, "\\1.0", "g", $2)}')"
+
 PAGER="cat" sudo checkinstall -y --type=debian --fstrans=no --nodoc \
 	--backup=no --deldoc=yes --install=no \
 	--pkgname="$PLUGIN_NAME" --pkgversion="$PKG_VERSION" \
 	--pkglicense="GPLv2.0" --maintainer="$LINUX_MAINTAINER_EMAIL" \
 	--pkggroup="video" \
-	--requires="obs-studio \(\>= 25.0.7\), libqt5core5a, libqt5widgets5, qt5-image-formats-plugins" \
+	--requires="obs-studio \(\>= ${OBS_VER}\), libqt5core5a, libqt5widgets5, qt5-image-formats-plugins" \
 	--pakdir="../package"
 
 sudo chmod ao+r ../package/*
