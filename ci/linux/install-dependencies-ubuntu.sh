a8168a64495603e61128548d8016b0a40759296c
Norihiro Kamae <norihiro@nagater.net>
2021-12-29 21:49:30 +0900

    ci(linux): Download obs-frontend-api.h from current version
    The version of obs-studio is retreived from dpkg and download from
    github.

--- ci/linux/install-dependencies-ubuntu.sh
+++ ci/linux/install-dependencies-ubuntu.sh
@@ -14,6 +14,8 @@ sudo apt-get install -y \
 	qtbase5-dev
 
 # Dirty hack
-sudo wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/25.0.0/UI/obs-frontend-api/obs-frontend-api.h
+obs_version="$(dpkg -s obs-studio | awk '$1=="Version:" {print gensub(/-.*/, "", 1, $2)}')"
+echo obs-studio version is "$obs_version"
+sudo wget -O /usr/include/obs/obs-frontend-api.h https://raw.githubusercontent.com/obsproject/obs-studio/${obs_version}/UI/obs-frontend-api/obs-frontend-api.h
 
 sudo ldconfig
