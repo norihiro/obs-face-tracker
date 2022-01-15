c9ce579ad91ca05294af3c185305e1ef6638c330
Norihiro Kamae <norihiro@nagater.net>
2022-01-15 11:34:35 +0900

    face-detector-dlib: Leave error log
    Leave a log message if the image is too small.

--- src/face-detector-dlib.cpp
+++ src/face-detector-dlib.cpp
@@ -7,12 +7,15 @@
 
 #include <dlib/image_processing/frontal_face_detector.h>
 
+#define MAX_ERROR 2
+
 struct face_detector_dlib_private_s
 {
 	texture_object *tex;
 	std::vector<rect_s> rects;
 	dlib::frontal_face_detector *detector;
 	int crop_l = 0, crop_r = 0, crop_t = 0, crop_b = 0;
+	int n_error = 0;
 	face_detector_dlib_private_s()
 	{
 		tex = NULL;
@@ -55,8 +58,16 @@ void face_detector_dlib::detect_main()
 		int x1 = img->nc() - (int)(p->crop_r / p->tex->scale);
 		y0 = (int)(p->crop_t / p->tex->scale);
 		int y1 = img->nr() - (int)(p->crop_b / p->tex->scale);
-		if (x1 - x0 < 80 || y1 - y0 < 80)
+		if (x1 - x0 < 80 || y1 - y0 < 80) {
+			if (p->n_error++ < MAX_ERROR)
+				blog(LOG_ERROR, "too small image: %dx%d cropped left=%d right=%d top=%d bottom=%d",
+						img->nc(), img->nr(),
+						p->crop_l, p->crop_r, p->crop_t, p->crop_b );
 			return;
+		}
+		else if (p->n_error) {
+			p->n_error--;
+		}
 		img_crop.set_size(y1 - y0, x1 - x0);
 		for (int y = y0; y < y1; y++) {
 			for (int x = x0; x < x1; x++) {
@@ -65,8 +76,14 @@ void face_detector_dlib::detect_main()
 		}
 		img = &img_crop;
 	}
-	if (img->nc()<80 || img->nr()<80)
+	if (img->nc()<80 || img->nr()<80) {
+		if (p->n_error++ < MAX_ERROR)
+			blog(LOG_ERROR, "too small image: %dx%d", img->nc(), img->nr());
 		return;
+	}
+	else if (p->n_error) {
+		p->n_error--;
+	}
 
 	if (!p->detector)
 		p->detector = new dlib::frontal_face_detector(dlib::get_frontal_face_detector());
