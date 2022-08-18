norihiro/obs-face-tracker

commit 1a79992678b4578f3172e343eafc74612abd9196

Not change zoom if scale_max changed
An internal parameter `range_min_out` is introduced for output zoom
range, which is calculated from current zoom and the `scale_max` in the
properties.

src/face-tracker.cpp
@@ -106,7 +106,7 @@ struct face_tracker_filter
 
 	rectf_s crop_cur;
 	f3 detect_err;
-	f3 range_min, range_max;
+	f3 range_min, range_max, range_min_out;
 
 	float upsize_l, upsize_r, upsize_t, upsize_b;
 	float track_z, track_x, track_y;
@@ -596,11 +596,11 @@ static void tick_filter(struct face_tracker_filter *s, float second)
 
 	for (int i=0; i<3; i++) {
 		if (isnan(u.v[i]))
-			u.v[i] = s->range_min.v[i];
-		else if (u.v[i] < s->range_min.v[i]) {
-			u.v[i] = s->range_min.v[i];
-			if (s->filter_int_out.v[i] < s->range_min.v[i])
-				s->filter_int_out.v[i] = s->range_min.v[i];
+			u.v[i] = s->range_min_out.v[i];
+		else if (u.v[i] < s->range_min_out.v[i]) {
+			u.v[i] = s->range_min_out.v[i];
+			if (s->filter_int_out.v[i] < s->range_min_out.v[i])
+				s->filter_int_out.v[i] = s->range_min_out.v[i];
 		}
 		else if (u.v[i] > s->range_max.v[i]) {
 			u.v[i] = s->range_max.v[i];
@@ -676,6 +676,8 @@ static void ftf_tick(void *data, float second)
 		s->range_max.v[1] = s->known_height - get_height(s->crop_cur) * 0.5f;
 		s->range_min.v[2] = sqrtf(s->known_width*s->known_height) / s->scale_max;
 		s->range_max.v[2] = sqrtf(s->width_with_aspect * s->height_with_aspect);
+		s->range_min_out = s->range_min;
+		s->range_min_out.v[2] = std::min(s->range_min.v[2], s->filter_int_out.v[2]);
 		calculate_error(s);
 		tick_filter(s, second);
 	}

commit 9e252f092561058e8782e4953c3d62a57e586f8a

face-tracker: Fix zoom becoming negative
A bug that zoom becomes negative was suspected to be introduced in
1a799926.

src/face-tracker.cpp
@@ -381,7 +381,7 @@ static void tick_filter(struct face_tracker_filter *s, float second)
 
 	for (int i=0; i<3; i++) {
 		if (isnan(u.v[i]))
-			u.v[i] = s->range_min_out.v[i];
+			u.v[i] = (s->range_max.v[i] + s->range_min_out.v[i]) * 0.5f;
 		else if (u.v[i] < s->range_min_out.v[i]) {
 			u.v[i] = s->range_min_out.v[i];
 			if (s->filter_int_out.v[i] < s->range_min_out.v[i])
@@ -393,6 +393,8 @@ static void tick_filter(struct face_tracker_filter *s, float second)
 				s->filter_int_out.v[i] = s->range_max.v[i];
 		}
 	}
+
+	s->u_last = u;
 
 	if (s->debug_data_control) {
 		fprintf(s->debug_data_control, "%f\t%f\t%f\t%f\n",
@@ -509,7 +511,7 @@ static void ft_tick_internal(struct face_tracker_filter *s, float second, bool w
 		s->range_min.v[2] = sqrtf(s->known_width*s->known_height) / s->scale_max;
 		s->range_max.v[2] = sqrtf(s->width_with_aspect * s->height_with_aspect);
 		s->range_min_out = s->range_min;
-		s->range_min_out.v[2] = std::min(s->range_min.v[2], s->filter_int_out.v[2]);
+		s->range_min_out.v[2] = std::max(std::min(s->range_min.v[2], s->u_last.v[2]), 1.0f);
 		calculate_error(s);
 		tick_filter(s, second);
 	}

src/face-tracker.hpp
@@ -34,6 +34,7 @@ struct face_tracker_filter
 	f3 filter_int_out;
 	f3 filter_int;
 	f3 filter_lpf;
+	f3 u_last;
 	int aspect_x, aspect_y;
 
 	// face tracker source